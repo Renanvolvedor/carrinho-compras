dad9b5d2d5f2374f6e23332cddbfa0531c307371
Renan Cardoso Segretti <renanony@gmail.com>
2024-02-19 08:50:04 -0300

    trabalhando na função adicionar()

--- js/app.js
+++ js/app.js
@@ -2,9 +2,8 @@ function adicionar () {
     let produto = document.getElementById('produto').value;
     let quantidade = document.getElementById('quantidade').value;
     let carrinho = document.getElementById('lista-produtos');
-    let quantidadeNoCarrinho = carrinho.querySelector('.texto-azul');
-    let produtoNoCarrinho = carrinho.querySelector('.carrinho__produtos__produto');
 
-    quantidadeNoCarrinho.textContent = quantidade;
-    produtoNoCarrinho.textContent = produto;
+    carrinho.querySelector('.carrinho__produtos__produto').textContent = produto;
+    carrinho.querySelector('.texto-azul').textContent = `${quantidade}x`;
+    
 }
